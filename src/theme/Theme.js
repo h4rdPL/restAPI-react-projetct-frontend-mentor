92591ba88fbf9ed1dd465916d3d5f1529ab8bb90
h4rdPL <mateusz.mamica18@gmail.com>
2022-09-08 09:27:20 +0200

    adding theme variables

---
+++ src/theme/Theme.js
@@ -0,0 +1,14 @@
+export const darkTheme = {
+  darkBlue: "hsl(209, 23%, 22%)", // elements
+  veryDarkBlueBg: "hsl(207, 26%, 17%)", // background
+};
+
+export const neutralTheme = {
+  white: "hsl(0, 0%, 100%)",
+};
+
+export const lightTheme = {
+  veryDarkBlueText: "hsl(200, 15%, 8%)", // text
+  darkGray: "hsl(0, 0%, 52%)", // input
+  veryLightGray: "hsl(0, 0%, 98%)", // background
+};
